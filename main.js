7240fc0b46090b88fa11907e7b908ec8b8f3231f
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-07 17:04:24 -0400

    Update File Structure

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const {token, prefix} = require('./config.json');
 
-const config = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () =>
 {
-    console.log (`${client.user.tag} has logged into Discord.`);
+    console.log(`${client.user.tag} has logged into Discord.`);
 });
 
-console.log(config.token);
+client.on('message', message =>
+{
+
+const args = message.content.slice(prefix.length).trim().split(/ +/);
+const command = args.shift().toLowerCase();
+
+});
+
+
+
+console.log(token);
