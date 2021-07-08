8db6f2074c38ec055bd6ba40d811b181a424ca86
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-07 22:12:35 -0400

    Update kick, ban

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -2,7 +2,10 @@ module.exports =
 {
     name: 'kick',
     description: 'Kicks the mentioned user from the server',
+    args: true,
+    usage: '<user>',
     guildOnly: true,
+    permissions: 'KICK_MEMBERS',
     execute(message, args)
     {
         const user = message.mentions.members.first();
