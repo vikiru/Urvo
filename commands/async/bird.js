65729278c54d0e987de1801ee50b402da46abedd
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-09 16:34:18 -0400

    Add random bird command

---
+++ commands/async/bird.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+    name: 'bird',
+    description: 'Sends the user a random picture of a bird',
+    args: false,
+    guildOnly: true,
+    exectute(message, args)
+    {
+    }
+}
