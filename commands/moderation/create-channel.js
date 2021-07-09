c5e759af135e3b284542c465755efa7322cac3f0
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-08 20:43:38 -0400

    Update create-channel.js

--- commands/moderation/create-channel.js
+++ commands/moderation/create-channel.js
@@ -3,11 +3,28 @@ module.exports =
     name: 'create-channel',
     description: 'Create a new channel of specified type',
     args: true,
-    usage: '<name>',
+    usage: '<name> <type>',
     permissions: 'MANAGE_CHANNELS',
     guildOnly: true,
     execute(message, args)
     {
-        message.guild.channels.create(args[0]);
+        message.guild.channels.create(args[0], 
+            {
+                type: args[1],
+            })
+            .then((channel) =>
+            {
+                const textID = '851621332587905085';
+                const voiceID = '851621332587905086';
+                switch (args[1])
+                {
+                    case 'text':
+                        channel.setParent(textID);
+                    break;
+                    case 'voice':
+                        channel.setParent(voiceID);
+                    break;
+                }
+            })
     }
 };
