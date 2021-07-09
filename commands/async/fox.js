03a754471ea795b5cfe98802943e24f473a0fd8c
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-09 16:15:01 -0400

    Create fox.js

---
+++ commands/async/fox.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+    name: 'fox',
+    description: 'Sends the user a random picture of a fox',
+    args: false,
+    guildOnly: true,
+    exectute(message, args)
+    {
+    }
+}
