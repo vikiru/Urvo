dfcaa7726ef601026ecad652a0fc8853c029575b
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-09 16:07:33 -0400

    Create cat.js

---
+++ commands/async/cat.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+    name: 'cat',
+    description: 'Sends the user a random picture of a cat',
+    args: false,
+    guildOnly: true,
+    exectute(message, args)
+    {
+    }
+}
