07a3f6c0ff38597b913b8abaddba0b2d7c07981f
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-09 16:07:29 -0400

    Create dog.js

---
+++ commands/async/dog.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+    name: 'dog',
+    description: 'Sends the user a random picture of a dog',
+    args: false,
+    guildOnly: true,
+    exectute(message, args)
+    {
+    }
+}
