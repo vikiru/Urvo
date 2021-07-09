a2958eef617b4d097537b9bc271e239db9284581
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-08 20:40:28 -0400

    Update main.js
    - add async command handling to send random cat image and anime from public rest api
    - update non async command handling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const {Collection, Client} = require('discord.js');
+const {Collection, Client, MessageEmbed} = require('discord.js');
 const {token, prefix} = require('./config.json');
 
 const client = new Client();
@@ -7,6 +7,8 @@ const client = new Client();
 client.commands = new Collection();
 
 const commandFolders = fs.readdirSync('./commands');
+
+const fetch = require('node-fetch');
 
 // Accessing the commands
 for (const folder of commandFolders)
@@ -20,6 +22,47 @@ for (const folder of commandFolders)
     }
 
 }
+
+// Handling async commands
+client.on('message', async message =>
+{
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    
+    const querystring = require ('querystring');
+    const query = querystring.stringify({query: args.join(' ')});
+    //const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str); 
+
+    if (commandName === 'cat')
+    {
+        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+        message.channel.send(file);
+    }
+
+    if (commandName === 'dog'){}
+
+    if (commandName === 'anime')
+    {
+        const { results } = await fetch(`https://api.jikan.moe/v3/search/${commandName}?q=${query}`).then(response => response.json());;
+        const animeInfo = results[0];
+        
+        const embed = new MessageEmbed()
+        .setColor('#EFFF00')
+        .setTitle(animeInfo.title)
+        .setURL(animeInfo.url)
+        .setThumbnail(animeInfo.image_url)
+        .addFields (
+            {name: 'Rating', value: animeInfo.rated, inline: true},
+            {name: 'Type', value: animeInfo.type, inline: true},
+            {name: 'Score', value: animeInfo.score, inline: true}, 
+            {name: 'Episodes', value: animeInfo.episodes, inline: true},
+            {name: 'Start Date', value: animeInfo.start_date = animeInfo.start_date.split('T')[0], inline: true},
+            {name: 'End Date', value: animeInfo.end_date = animeInfo.end_date.split('T')[0], inline: true},
+            {name: 'Description', value: animeInfo.synopsis}
+        );
+        message.channel.send(embed);
+    }
+});
 
 // Handling Commands
 client.on('message', message =>
@@ -36,13 +79,14 @@ if (command != undefined && command.guildOnly && message.channel.type === 'dm')
     return message.reply('The specified command is not meant to be used in DMS');
 }
 
+if (args && command != undefined)
+{
 try{
     if (command.name != 'help') command.execute(message, args);
     else command.execute(message);
 } catch (commandError) {
     console.error(commandError);
-    if (!client.commands.has(commandName)) message.reply('The entered command does not exist');
-    else if (command != undefined && command.args && !args.length)
+    if (command != undefined && command.args && !args.length)
     {
         let reply = `You didn't provide any arguments, ${message.author}`;
         if (command.usage)
@@ -54,11 +98,16 @@ try{
     }
     else message.reply('There was an error executing the specified command');
 }
+}
+else message.reply('The entered command does not exist');
+
+
 });
 
 client.login(token);
 client.on ('ready', () =>
 {
     console.log(`${client.user.tag} has logged in`);
+    //client.user.setAvatar('https://image.winudf.com/v2/image/Y29tLmNvb2xib3lhbmltZS5zeW1waG9ueV9zY3JlZW5fM18xNTI4MDc4Mjg4XzAwMw/screen-3.jpg?fakeurl=1&type=.jpg');
     client.user.setPresence({activity: {name: 'Evolving...'}, status: 'online'});
 })
