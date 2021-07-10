4f941672667dc0c43671572463f38e89c5eb02e2
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-09 23:58:43 -0400

    Update cat.js

--- commands/async/cat.js
+++ commands/async/cat.js
@@ -1,9 +1,27 @@
+const { MessageEmbed } = require('discord.js'); 
+async function randomCat(message, args)
+{
+    try
+    {
+    const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+    const embed = new MessageEmbed()
+    .setTitle('Random Cat!')
+    .setColor('#EFFF00')
+    .setImage(file);
+    message.channel.send(embed);
+    } catch (error)
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
     name: 'cat',
     description: 'Sends the user a random picture of a cat',
     guildOnly: true,
-    exectute(message, args)
+    execute(message, args)
     {
+        randomCat(message,args);
     }
 }
