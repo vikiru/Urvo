1bc3b1f20f20e7e0f7b90ddc0120acd069b4e6bb
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-10 00:33:48 -0400

    Update kangaroo.js

--- commands/async/kangaroo.js
+++ commands/async/kangaroo.js
@@ -1,3 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+async function randomKangaroo(message, args)
+{
+    try
+    {
+        const { link } = await fetch('https://some-random-api.ml/img/kangaroo').then(response => response.json());
+
+        const embed = new MessageEmbed()
+        .setTitle('Random Kangaroo!')
+        .setColor('#EFFF00')
+        .setImage(link);
+        message.channel.send(embed);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
 module.exports = 
 {
     name: 'kangaroo',
@@ -5,5 +24,6 @@ module.exports =
     guildOnly: true,
     exectute(message, args)
     {
+        randomKangaroo(message, args);
     }
 }
