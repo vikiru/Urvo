fe3ec2f266eeab9364782206759b0877f1c8c541
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-10 00:26:40 -0400

    Update joke.js

--- commands/async/joke.js
+++ commands/async/joke.js
@@ -1,3 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+
+async function randomJoke(message, args)
+{
+    try
+    {
+        const jokes = await fetch('https://official-joke-api.appspot.com/jokes/ten').then(response => response.json());
+        
+        const embed = new MessageEmbed()
+        .setColor('#EFFF00')
+        .setTitle('Random Joke!')
+        .setThumbnail('https://logos.textgiraffe.com/logos/logo-name/Joke-designstyle-summer-m.png')
+        .addFields( 
+            {name: 'Setup', value: jokes[0].setup},
+            {name: 'Punchline', value: jokes[0].punchline}
+        );
+        message.channel.send(embed);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
     name: 'joke',
