d4d6cb43a5eac904d0dbf8f81807542e81afc356
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-10 00:46:39 -0400

    Update meal.js

--- commands/async/meal.js
+++ commands/async/meal.js
@@ -4,8 +4,10 @@ async function randomMeal(message, args)
 {
     try
     {
+        const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+
         const { meals } = await fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(response => response.json());
-                
+               
         const embed = new MessageEmbed()
         .setTitle(meals[0].strMeal)
         .setColor('#EFFF00')
@@ -13,8 +15,8 @@ async function randomMeal(message, args)
         .setURL(meals[0].strSource)
         .addFields(
             {name: 'Category', value: meals[0].strCategory, inline: true},
-            {name: 'Area', value: trim(meals[0].strArea, 1024), inline: true},
-            {name: 'Instructions', value: meals[0].strInstructions}
+            {name: 'Area', value: meals[0].strArea, inline: true},
+            {name: 'Instructions', value: trim(meals[0].strInstructions, 1024)}
         );
         message.channel.send(embed);
     }
