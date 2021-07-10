39a90ad8259345f5da6090f27105729ca3e4d4c2
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-10 00:07:46 -0400

    Update bird.js

--- commands/async/bird.js
+++ commands/async/bird.js
@@ -1,9 +1,29 @@
+const { MessageEmbed } = require('discord.js'); 
+
+async function randomBird(message, args)
+{
+    try 
+    {
+    const { link } = await fetch('https://some-random-api.ml/img/birb').then(response => response.json());
+        
+    const embed = new MessageEmbed()
+    .setTitle('Random Bird!')
+    .setColor('#EFFF00')
+    .setImage(link);
+    message.channel.send(embed);
+    } catch (error) 
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
     name: 'bird',
     description: 'Sends the user a random picture of a bird',
     guildOnly: true,
-    exectute(message, args)
+    execute(message, args)
     {
+        randomBird(message,args);
     }
 }
