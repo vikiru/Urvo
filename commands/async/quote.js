e3db661f7af9975d4f713be04803164e0efb692e
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-10 00:36:28 -0400

    Update quote.js

--- commands/async/quote.js
+++ commands/async/quote.js
@@ -1,3 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+
+async function randomQuote(message, args)
+{
+    try
+    {
+        const quote = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json').then(response => response.json());
+
+        const embed = new MessageEmbed()
+        .setTitle('Random Quote!')
+        .setColor('#EFFF00')
+        .setURL(quote.quoteLink)
+        .addFields(
+            {name: 'Quote', value: quote.quoteText},
+            {name: 'Author', value: quote.quoteAuthor}
+        );
+        message.channel.send(embed);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
     name: 'quote',
@@ -5,5 +29,6 @@ module.exports =
     guildOnly: true,
     exectute(message, args)
     {
+        randomQuote(message, args);
     }
 }
