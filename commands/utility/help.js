28f1d397ab353fd3ec43797c08f6439cbfd5ab61
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-16 22:07:53 -0400

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -35,21 +35,5 @@ module.exports =
     execute(message, args)
     {
         help(message, args);
-        /*
-        const helpEmbed = new MessageEmbed()
-        .setTitle('List of Commands')
-        .setColor('#EFFF00')
-        .setThumbnail('http://www.clker.com/cliparts/P/t/7/o/9/W/help-hi.png')
-        .setDescription('All of the possible commands that can be used')
-        .addFields(
-            {name: 'fun' , value: 'anime, bird, cat, dog, fox, joke, kangaroo, koala, meal, panda, quote'},
-            {name: 'moderation', value: 'ban, create-channel, delete-channel, kick, unban'},
-            {name: 'utility', value: 'help, prune'}
-            )
-        .setTimestamp()
-        .setFooter('Thanks!');
-        message.channel.send(helpEmbed);
-            */
-
     }
 }
