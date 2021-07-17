6bd678157469e1e9b73f32b36b9fa12f071cc0a2
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-16 22:31:26 -0400

    Update help.js
    - separate help command into categories based on command folder subfolder names

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -4,13 +4,22 @@ const { prefix } = require('../../config.json');
 function help(message, args)
 {
     const data = [];
-    const { asyncCommands, commands } = message.client;
+    const { asyncCommands, minigames, moderation, utility } = message.client;
+
+    const asyn = [];
+    const mg = [];
+    const mod = [];
+    const util = [];
 
     if (!args.length)
     {
-        data.push(asyncCommands.map(command => command.name).join(', '));
-        data.push(commands.map(command => command.name).join(', '));
+        asyn.push(asyncCommands.map(command => command.name).join(', '));
+        mg.push(minigames.map(command => command.name).join(', '));
+        mod.push(moderation.map(command => command.name).join(', '));
+        util.push(utility.map(command => command.name).join(', '));
+
         data.push(`\nYou can send ${prefix}help [command name] to retrieve all of the info on the specific command`);
+
 
         const helpEmbed = new MessageEmbed()
         .setTitle('List of Commands')
@@ -18,7 +27,11 @@ function help(message, args)
         .setThumbnail('http://www.clker.com/cliparts/P/t/7/o/9/W/help-hi.png')
         .setDescription('All of the possible commands that can be used')
         .addFields(
-            {name: 'Commands', value: data}
+            {name: 'Fun', value: asyn},
+            {name: 'Minigames', value: mg},
+            {name: 'Moderation', value: mod},
+            {name: 'Utility', value: util},
+            {name: '\u200b' , value: data}
         )
         .setTimestamp()
         .setFooter(`Prefix: ${prefix}`);
