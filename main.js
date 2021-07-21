325ac65cd78ac4059f7f109e73485a5323be927a
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-21 17:37:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const { Collection, Client } = require('discord.js');
 const { token, prefix } = require('./config.json');
-const { fips } = require('crypto');
 
 global.fetch = require('node-fetch');
 
 global.client = new Client();
+const queue = new Map();
 
 client.commands = new Collection();
-
+//-----------------------------------------//
 client.commands.fun = new Collection();
 client.commands.image = new Collection();
 client.commands.minigames = new Collection();
 client.commands.moderation = new Collection();
+client.commands.music = new Collection();
 client.commands.utility = new Collection();
+//-----------------------------------------//
 
 const commandFolders = fs.readdirSync('./commands');
 const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
@@ -39,6 +41,9 @@ for (const folder of commandFolders) {
 				break;
 			case 'moderation':
 				client.commands.moderation.set(command.name, command);
+				break;
+			case 'music':
+				client.commands.music.set(command.name, command);
 				break;
 			case 'utility':
 				client.commands.utility.set(command.name, command);
