c7ad3957fd5138e0b524da517f90f178b4eb7111
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-22 18:09:49 -0400

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,10 +1,24 @@
-const queuePlayer = require('./queueSetup');
+async function pauseQueue(message, args) {
+	const guild = message.guild;
+	const serverQ = queue.get(guild.id);
+
+	if (!serverQ.songs) {
+		serverQ.textChannel.send('There are no songs in the queue!');
+		return;
+	}
+
+	if (serverQ.songs.length != 0) {
+		const connection = serverQ.connection;
+		const dispatcher = connection.dispatcher;
+		dispatcher.pause();
+	}
+}
 
 module.exports = {
 	name: 'pause',
 	guildOnly: true,
 	permissions: ['CONNECT', 'SPEAK'],
 	execute(message, args) {
-		queuePlayer.pauseQueue(message, args);
+		pauseQueue(message, args);
 	},
 };
