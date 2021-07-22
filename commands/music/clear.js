2e920060f746906770b02f70960ab0b9a00ae1db
vikiru <viskirubakaran@cmail.carleton.ca>
2021-07-22 18:09:22 -0400

    Create clear.js
    - add clear command

---
+++ commands/music/clear.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js');
+
+function clearQueue(message, guild) {
+	const serverQ = queue.get(guild.id);
+
+	if (typeof serverQ === 'undefined') {
+		const emptyQueue = new MessageEmbed().setColor('#EFFF00').setDescription('**The queue is currently empty!**');
+		message.channel.send(emptyQueue);
+	} else {
+		serverQ.connection.dispatcher.pause();
+
+		for (song of serverQ.songs) {
+			serverQ.songs.pop();
+		}
+		serverQ.voiceChannel.leave();
+
+		const clearedQueue = new MessageEmbed().setColor('#EFFF00').setDescription('**The queue has been cleared!**');
+		message.channel.send(clearedQueue);
+	}
+}
+
+module.exports = {
+	name: 'clear',
+	guildOnly: true,
+	execute(message, args) {
+		clearQueue(message, message.guild);
+	},
+};
