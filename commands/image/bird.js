30d6d3e2b543ff99aac26873e9869b11b9bc348d
vikiru <viskirubakaran@cmail.carleton.ca>
2021-09-15 11:59:56 -0400

    Update bird.js

--- commands/image/bird.js
+++ commands/image/bird.js
@@ -1,22 +1,24 @@
 const { MessageEmbed } = require('discord.js');
 
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 // Fetch a random bird and send into the chat via an embed
 async function randomBird(message, args) {
-  try {
-    const { link } = await fetch('https://some-random-api.ml/img/birb').then((response) => response.json());
+	try {
+		const { link } = await fetch('https://some-random-api.ml/img/birb').then((response) => response.json());
 
-    const embed = new MessageEmbed().setTitle('Random Bird!').setColor('#EFFF00').setImage(link);
-    message.channel.send(embed);
-  } catch (error) {
-    console.log(error);
-  }
+		const birdEmbed = new MessageEmbed().setTitle('Random Bird!').setColor('#EFFF00').setImage(link);
+		message.channel.send({ embeds: [birdEmbed] });
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 module.exports = {
-  name: 'bird',
-  description: 'Sends the user a random picture of a bird',
-  guildOnly: true,
-  execute(message, args) {
-    randomBird(message, args);
-  }
+	name: 'bird',
+	description: 'Sends the user a random picture of a bird',
+	guildOnly: true,
+	execute(message, args) {
+		randomBird(message, args);
+	},
 };
