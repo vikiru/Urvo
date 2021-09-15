b9d5906505596822b8bc751177d71f081398d542
vikiru <viskirubakaran@cmail.carleton.ca>
2021-09-15 12:00:03 -0400

    Update fox.js

--- commands/image/fox.js
+++ commands/image/fox.js
@@ -1,22 +1,24 @@
 const { MessageEmbed } = require('discord.js');
 
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 // Fetch a random fox and send into the chat via an embed
 async function randomFox(message, args) {
-  try {
-    const { image } = await fetch('https://randomfox.ca/floof/').then((response) => response.json());
+	try {
+		const { image } = await fetch('https://randomfox.ca/floof/').then((response) => response.json());
 
-    const embed = new MessageEmbed().setTitle('Random Fox!').setColor('#EFFF00').setImage(image);
-    message.channel.send(embed);
-  } catch (error) {
-    console.log(error);
-  }
+		const foxEmbed = new MessageEmbed().setTitle('Random Fox!').setColor('#EFFF00').setImage(image);
+		message.channel.send({ embeds: [foxEmbed] });
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 module.exports = {
-  name: 'fox',
-  description: 'Sends the user a random picture of a fox',
-  guildOnly: true,
-  execute(message, args) {
-    randomFox(message, args);
-  }
+	name: 'fox',
+	description: 'Sends the user a random picture of a fox',
+	guildOnly: true,
+	execute(message, args) {
+		randomFox(message, args);
+	},
 };
