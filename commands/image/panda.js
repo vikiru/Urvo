8364f84e77ac5bab2c67f1982e91cb0fa4f1d3e9
vikiru <viskirubakaran@cmail.carleton.ca>
2021-09-15 12:00:15 -0400

    Update panda.js

--- commands/image/panda.js
+++ commands/image/panda.js
@@ -1,22 +1,24 @@
 const { MessageEmbed } = require('discord.js');
 
-// Fetch a random panaa and send into the chat via an embed
-async function randomPanda(message, args) {
-  try {
-    const { link } = await fetch('https://some-random-api.ml/img/panda').then((response) => response.json());
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-    const embed = new MessageEmbed().setTitle('Random Panda!').setColor('#EFFF00').setImage(link);
-    message.channel.send(embed);
-  } catch (error) {
-    console.log(error);
-  }
+// Fetch a random panda and send into the chat via an embed
+async function randomPanda(message, args) {
+	try {
+		const { link } = await fetch('https://some-random-api.ml/img/panda').then((response) => response.json());
+
+		const pandaEmbed = new MessageEmbed().setTitle('Random Panda!').setColor('#EFFF00').setImage(link);
+		message.channel.send({ embeds: [pandaEmbed] });
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 module.exports = {
-  name: 'panda',
-  description: 'Sends the user a random picture of a panda',
-  guildOnly: true,
-  execute(message, args) {
-    randomPanda(message, args);
-  }
+	name: 'panda',
+	description: 'Sends the user a random picture of a panda',
+	guildOnly: true,
+	execute(message, args) {
+		randomPanda(message, args);
+	},
 };
