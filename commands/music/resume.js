769202dac39e5edf5544426c1f7a1b2d69e3b920
vikiru <viskirubakaran@cmail.carleton.ca>
2021-09-17 16:18:16 -0400

    Update resume.js
    - working now with discord.js 13.1 and @discordjs/voice

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -1,10 +1,18 @@
-const queuePlayer = require('./queueSetup');
+// Resumes playing of the current song in the queue
+async function resumeQueue(message, args) {
+	const guild = message.guild;
+	const serverQ = queue.get(guild.id);
+
+	if (serverQ.songs.length > 0 && serverQ.connection != null) {
+		serverQ.player.unpause();
+	}
+}
 
 module.exports = {
 	name: 'resume',
 	guildOnly: true,
 	permissions: ['CONNECT', 'SPEAK'],
 	execute(message, args) {
-		queuePlayer.resumeQueue(message, args);
+		resumeQueue(message, args);
 	},
 };
