cab528e2b58c003651a3a4c35e3f3723d3829cdb
vikiru <viskirubakaran@cmail.carleton.ca>
2021-09-17 16:19:40 -0400

    Update clear.js
    - updated for discord.js 13.1.0

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -1,20 +1,27 @@
 const { MessageEmbed } = require('discord.js');
 
+// Clears all the songs in the queue
 function clearQueue(message, guild) {
 	const serverQ = queue.get(guild.id);
 
+	// No songs in queue
 	if (typeof serverQ === 'undefined') {
 		const emptyQueue = new MessageEmbed().setColor('#EFFF00').setDescription('**The queue is currently empty!**');
-		message.channel.send(emptyQueue);
-	} else {
-		serverQ.connection.dispatcher.pause();
+		message.channel.send({ embeds: [emptyQueue] });
+	}
+
+	// Songs exist in queue
+	else {
+		if (typeof serverQ != undefined && serverQ.connection) {
+			serverQ.player.pause();
+		}
 
 		for (song of serverQ.songs) {
 			serverQ.songs.pop();
 		}
 
 		const clearedQueue = new MessageEmbed().setColor('#EFFF00').setDescription('**The queue has been cleared!**');
-		message.channel.send(clearedQueue);
+		message.channel.send({ embeds: [clearedQueue] });
 	}
 }
 
