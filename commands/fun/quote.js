f4748eba6af7e2897d6ac7e8361fa191940e1a92
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-16 19:42:18 -0400

    Update quote.js
    - fix to work with discord.js v14
    - updated look of embed

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -1,29 +1,28 @@
-const { MessageEmbed, SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
-// Fetch a random quote and send into the chat via an embed
-async function randomQuote(message, args) {
-	try {
-		const quote = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json').then(
-			(response) => response.json(),
-		);
-
-		const quoteEmbed = new MessageEmbed()
-			.setTitle('Random Quote!')
-			.setColor('#EFFF00')
-			.setURL(quote.quoteLink)
-			.addFields({ name: 'Quote', value: quote.quoteText }, { name: 'Author', value: quote.quoteAuthor });
-		message.channel.send({ embeds: [quoteEmbed] });
-	} catch (error) {
-		console.log(error);
-	}
-}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('quote').setDescription('Send a random quote'),
 	guildOnly: true,
-	execute(message, args) {
-		randomQuote(message, args);
+	/**
+	 * Fetch a random quote and send into the chat via an embed
+	 * @param {*} interaction
+	 */
+	async execute(interaction) {
+		const randomQuote = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json').then(
+			(response) => response.json(),
+		);
+
+		const quoteMsg = quote(randomQuote.quoteText) + '\n' + `~${randomQuote.quoteAuthor}`;
+
+		const quoteEmbed = new EmbedBuilder()
+			.setTitle('Random Quote!')
+			.setDescription(quoteMsg)
+			.setColor('#b35843')
+			.setTimestamp()
+			.setURL(quote.quoteLink)
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		interaction.reply({ embeds: [quoteEmbed] });
 	},
 };
