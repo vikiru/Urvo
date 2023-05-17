7deec15bf3a5f3df9f5afdd1a5890138c1ac77ae
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-17 17:37:30 -0400

    Update anime-quote.js
    - add comment and updated embed

--- commands/fun/anime-quote.js
+++ commands/fun/anime-quote.js
@@ -3,21 +3,26 @@ const { URLSearchParams } = require('url');
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-// Fetch a random anime quote, then use the anime name from the random quote
-// to search for the MAL anime listing
+//
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('anime-quote').setDescription('Send a random quote from an anime'),
 	guildOnly: true,
+	/**
+	 * Fetch a random anime quote, then use the anime name from the random quote
+	 * to search for the MAL anime listing
+	 * @param {*} interaction
+	 */
 	async execute(interaction) {
 		const quoteData = await fetch('https://animechan.vercel.app/api/random').then((response) => response.json());
 		const animeQuote = quote('"' + quoteData.quote + '"');
 
 		const quoteEmbed = new EmbedBuilder()
-			.setColor('#EFFF00')
+			.setColor('#b35843')
 			.setTitle(`Random Quote from ${quoteData.anime}`)
 			.setTimestamp()
-			.addFields({ name: 'Quote', value: animeQuote }, { name: 'Character', value: quoteData.character });
+			.addFields({ name: 'Quote', value: animeQuote }, { name: 'Character', value: quoteData.character })
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 		interaction.reply({ embeds: [quoteEmbed] });
 	},
 };
