38cf0dd61dbbe04cc554e74266ef847b467bd2a6
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-19 02:22:04 -0400

    Update joke.js
    - fix to work with discord.js v14
    - updated look of embed

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,28 +1,28 @@
-const { MessageEmbed, SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
-// Fetch a random joke and send into the chat via an embed
-async function randomJoke(message, args) {
-	try {
-		// TODO (NEED TO UPDATE API / API ERROR): const jokes = await fetch('https://official-joke-api.appspot.com/jokes/ten').then((response) => response.json());
-
-		const jokeEmbed = new MessageEmbed()
-			.setTitle('Random Joke!')
-			.setColor('#EFFF00')
-			.setThumbnail('https://logos.textgiraffe.com/logos/logo-name/Joke-designstyle-summer-m.png')
-			.addFields({ name: 'Setup', value: jokes[0].setup }, { name: 'Punchline', value: jokes[0].punchline });
-
-		message.channel.send({ embeds: [jokeEmbed] });
-	} catch (error) {
-		console.log(error);
-	}
-}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('joke').setDescription('Receive a random joke'),
 	guildOnly: true,
-	execute(message, args) {
-		randomJoke(message, args);
+	/**
+	 * Fetch a random joke and send into the chat via an embed
+	 * @param {*} interaction
+	 */
+	async execute(interaction) {
+		const randomJoke = await fetch('https://official-joke-api.appspot.com/jokes/random').then((response) =>
+			response.json(),
+		);
+
+		const text = randomJoke.setup + '\n' + randomJoke.punchline;
+
+		const jokeEmbed = new EmbedBuilder()
+			.setTitle('Random Joke')
+			.setColor('#b35843')
+			.setTimestamp()
+			.setThumbnail('https://logos.textgiraffe.com/logos/logo-name/Joke-designstyle-summer-m.png')
+			.setDescription(text)
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		interaction.reply({ embeds: [jokeEmbed] });
 	},
 };
