cf1d4fc6adc4e142ffe4475ba0825881e06c6b6d
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-20 19:23:12 -0400

    Update ban.js
    - fix to work with discord.js v14

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,22 +1,56 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	ButtonStyle,
+	ActionRowBuilder,
+	ButtonBuilder,
+} = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('ban').setDescription('Bans the mentioned user from the server'),
-	args: true,
-	usage: `<user>`,
-	permissions: 'BAN_MEMBERS',
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Bans the mentioned user from the server')
+		.addUserOption((option) =>
+			option.setName('user').setDescription('The user to ban from the server').setRequired(true),
+		)
+		.addStringOption((option) =>
+			option.setName('reason').setDescription('The reason to ban the user from the server').setRequired(false),
+		)
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 	guildOnly: true,
-	execute(message, args) {
-		const user = message.mentions.users.first();
-		console.log(args);
-		if (user.id === message.author.id) message.reply(`You can't ban yourself`);
-		else if (
-			user != undefined &&
-			user != message.author &&
-			message.author.hasPermission('BAN_MEMBERS') &&
-			client.hasPermission('BAN_MEMBERS')
-		) {
-			message.guild.ban(user);
-		} else message.reply('You need to mention a user');
+	async execute(interaction) {
+		const targetUser = interaction.options.getUser('user');
+		const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+		const confirmBan = new ButtonBuilder().setCustomId('confirm').setLabel('Confirm').setStyle(ButtonStyle.Danger);
+		const cancelBan = new ButtonBuilder().setCustomId('cancel').setLabel('Cancel').setStyle(ButtonStyle.Secondary);
+		const row = new ActionRowBuilder().addComponents(cancelBan, confirmBan);
+
+		const response = await interaction.reply({
+			content: `Are you sure you want to ban <@${targetUser.id}> from the server?`,
+			components: [row],
+		});
+		const filter = (i) => i.user.id === interaction.user.id;
+		try {
+			const confirmation = await response.awaitMessageComponent({ filter: filter, time: 60_000 });
+			const confirmId = confirmation.customId;
+			if (confirmId === 'confirm') {
+				await interaction.guild.bans.create(targetUser.id);
+				await interaction.editReply({
+					content: `Successfully banned <@${targetUser.id}> for ${reason}. As requested by ${interaction.user.username}`,
+					components: [],
+				});
+			} else if (confirmId === 'cancel') {
+				await confirmation.update({
+					content: `Aborting command execution for banning of <@${targetUser.id}>`,
+					components: [],
+				});
+			}
+		} catch (error) {
+			await interaction.editReply({
+				content: `Confirmation to ban <@${targetUser.id}> was not received within 1 minute. Aborting command execution.`,
+				components: [],
+			});
+		}
 	},
 };
