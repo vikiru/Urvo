b4864a043506e55701e55381c299afea7ec2b144
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-21 01:49:07 -0400

    Update create-channel.js
    - fix to work with discord.js v14
    todo: assign to parent category

--- commands/moderation/create-channel.js
+++ commands/moderation/create-channel.js
@@ -1,44 +1,71 @@
-const { CategoryChannel, Collection, SlashCommandBuilder } = require('discord.js');
+const {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	ChannelType,
+} = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('create-channel')
-		.setDescription('Create a new channel of the specified type'),
-	args: true,
-	usage: '<name> <type>',
-	permissions: 'MANAGE_CHANNELS',
+		.setDescription('Create a new channel of the specified type')
+		.addStringOption((option) =>
+			option.setName('channel_name').setDescription('The name of the new channel').setRequired(true),
+		)
+		.addStringOption((option) =>
+			option
+				.setName('channel_type')
+				.setDescription('The type of channel to create')
+				.addChoices({ name: 'Text Channel', value: 'text_channel' }, { name: 'Voice Channel', value: 'voice_channel' })
+				.setRequired(true),
+		)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
 	guildOnly: true,
-	execute(message, args) {
-		message.guild.channels
-			.create(args[0], {
-				type: args[1],
-			})
-			.then((channel) => {
-				const channels = message.guild.channels.cache;
-				const categories = new Collection();
+	/**
+	 *
+	 * @param {*} interaction
+	 */
+	async execute(interaction) {
+		const channelName = interaction.options.getString('channel_name');
+		const channelType = interaction.options.getString('channel_type');
 
-				// Add all the categories of the server into the categories collection
-				for (c of channels) {
-					if (c[1].type === 'category') {
-						categories.set(c[1].name, c[1].id);
-					}
-				}
+		const confirmCreate = new ButtonBuilder().setCustomId('confirm').setLabel('Confirm').setStyle(ButtonStyle.Danger);
+		const cancelCreate = new ButtonBuilder().setCustomId('cancel').setLabel('Cancel').setStyle(ButtonStyle.Secondary);
+		const row = new ActionRowBuilder().addComponents(cancelCreate, confirmCreate);
 
-				// Assign the newly created channel to Text Channels or Voice Channels
-				// depending on the type, if either category does not exist
-				// then add channel to the first category
-				switch (args[1]) {
-					case 'text':
-						const textID = categories.get('Text Channels');
-						if (textID === undefined) channel.setParent(categories.first());
-						else channel.setParent(textID);
-						break;
-					case 'voice':
-						const voiceID = categories.get('Voice Channels');
-						if (voiceID === undefined) channel.setParent(categories.first());
-						else channel.setParent(voiceID);
-						break;
+		const response = await interaction.reply({
+			content: `Are you sure you want to create a new ${channelType.replace('_', ' ')} named ${channelName}?`,
+			components: [row],
+		});
+		const filter = (i) => i.user.id === interaction.user.id;
+
+		try {
+			const confirmation = await response.awaitMessageComponent({ filter: filter, time: 60_000 });
+			const confirmId = confirmation.customId;
+
+			if (confirmId === 'confirm') {
+				if (channelType === 'text_channel') {
+					await interaction.guild.channels.create({ name: channelName, type: ChannelType.GuildText });
+				} else if (channelType === 'voice_channel') {
+					await interaction.guild.channels.create({ name: channelName, type: ChannelType.GuildVoice });
 				}
+				await interaction.editReply({
+					content: `Successfully created new channel. As requested by <@${interaction.user.id}>`,
+					components: [],
+				});
+			} else if (confirmId === 'cancel') {
+				await confirmation.update({
+					content: `Aborting command execution for channel creation`,
+					components: [],
+				});
+			}
+		} catch (error) {
+			await interaction.editReply({
+				content: `Confirmation to create channel was not received within 1 minute. Aborting command execution.`,
+				components: [],
 			});
+		}
 	},
 };
