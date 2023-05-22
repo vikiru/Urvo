a7321f34775c417af44257db489729e516aad310
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-22 02:32:31 -0400

    Create affirmations.js
    - create a random affirmation command

---
+++ commands/fun/affirmations.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('affirmation').setDescription('Send a random affirmation'),
+	guildOnly: true,
+	/**
+	 * Fetch a random affirmation and send it as an embed into the chat.
+	 * @param {*} interaction
+	 */
+	async execute(interaction) {
+		const randomAffirmation = await fetch('https://www.affirmations.dev').then((response) => response.json());
+
+		const affirmationEmbed = new EmbedBuilder()
+			.setTitle(`☀️ Random Affirmation!`)
+			.setColor('#b35843')
+			.setImage('https://cdn.pixabay.com/photo/2014/04/21/07/00/field-328962_1280.jpg')
+			.setTimestamp()
+			.addFields({ name: 'Lines', value: randomAffirmation.affirmation })
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+		interaction.reply({ embeds: [affirmationEmbed] });
+	},
+};
