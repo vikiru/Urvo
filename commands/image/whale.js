fbbb9561c064f19b04580d41f86a3b76f13e3306
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-21 23:59:57 -0400

    Update whale.js
    - fix typo

--- commands/image/whale.js
+++ commands/image/whale.js
@@ -3,17 +3,17 @@ const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('red_panda').setDescription('Send a random image of a red panda'),
+	data: new SlashCommandBuilder().setName('whale').setDescription('Send a random image of a whale'),
 	guildOnly: true,
 	/**
-	 * Fetch a random red panda and send into the chat via an embed
+	 * Fetch a random whale and send into the chat via an embed
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
 		const randomWhale = await fetch('https://some-random-api.com/animal/whale').then((response) => response.json());
 
 		const whaleEmbed = new EmbedBuilder()
-			.setTitle('Random Whale!')
+			.setTitle('🐋 Random Whale!')
 			.setColor('#b35843')
 			.setTimestamp()
 			.setImage(randomWhale.image)
