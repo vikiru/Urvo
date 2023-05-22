3971cbb782e8572a2a828a5695d6b59af12677da
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-22 02:25:19 -0400

    Update shakespeare.js

--- commands/fun/shakespeare.js
+++ commands/fun/shakespeare.js
@@ -15,14 +15,14 @@ module.exports = {
 		const randomSonnet = await fetch('https://poetrydb.org/author,title,random/Shakespeare;Sonnet;1').then((response) =>
 			response.json(),
 		);
-		const animeQuote = quote('"' + randomSonnet.quote + '"');
 
-		const quoteEmbed = new EmbedBuilder()
-			.setTitle(`✍️ ${randomSonnet.title}`)
+		const sonnetEmbed = new EmbedBuilder()
+			.setTitle(`✍️ ${randomSonnet[0].title}`)
 			.setColor('#b35843')
 			.setTimestamp()
-			.addFields({ name: 'Lines', value: randomSonnet.lines.join('\n') })
+			.addFields({ name: 'Lines', value: randomSonnet[0].lines.join('\n') })
 			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-		interaction.reply({ embeds: [quoteEmbed] });
+		
+		interaction.reply({ embeds: [sonnetEmbed] });
 	},
 };
