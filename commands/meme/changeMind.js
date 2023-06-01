2bf2762211efecc928e3d652e505a96209fb4d03
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-01 15:39:10 -0400

    Create changeMind.js
    - create a change my mind meme command

---
+++ commands/meme/changeMind.js
@@ -0,0 +1,35 @@
+const { EmbedBuilder, SlashCommandBuilder, quote, Client } = require('discord.js');
+
+const fetch = (...args) =>
+    import ('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('change_my_mind')
+        .setDescription('Send a Change My Mind meme with your desired text')
+        .addStringOption((option) =>
+            option.setName('top_text').setDescription('Text that appears on the top').setRequired(true),
+        )
+        .addStringOption((option) =>
+            option.setName('bottom_text').setDescription('Text that appears on the bottom').setRequired(true),
+        ),
+    guildOnly: true,
+    /**
+     * Sends a Change My Mind meme with the specified text
+     * @param {*} interaction
+     */
+    async execute(interaction) {
+        const topText = interaction.options.getString('top_text');
+        const bottomText = interaction.options.getString('bottom_text');
+
+        const mindEmbed = new EmbedBuilder()
+            .setTitle('Change My Mind Meme')
+            .setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
+            .setColor('#b35843')
+            .setTimestamp()
+            .setImage(`https://api.memegen.link/images/cmm/${topText}/${bottomText}`)
+            .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+        interaction.reply({ embeds: [mindEmbed] });
+    },
+};
