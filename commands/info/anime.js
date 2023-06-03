f8a0ef350ff5c079d57866b43b26d4de1c6fd76a
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-03 17:37:41 -0400

    Update anime.js
    - improved search result for anime command

--- commands/info/anime.js
+++ commands/info/anime.js
@@ -4,6 +4,28 @@ const { URLSearchParams } = require('url');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
+/**
+ * Iterates through the search result data to find the first anime matching the desired anime title
+ * @param {*} data
+ * @param {*} desiredAnime
+ * @returns The data corresponding to the desired anime
+ */
+function findAnime(data, desiredAnime) {
+	let index = 0;
+	let animeTitle = desiredAnime.toLowerCase();
+	for (const currentData of data) {
+		let searchIndex = currentData.titles.findIndex(
+			(titleEntry) =>
+				titleEntry.title.toLowerCase() === animeTitle || titleEntry.title.toLowerCase().includes(animeTitle),
+		);
+		if (searchIndex !== -1) {
+			index = data.indexOf(currentData);
+			break;
+		}
+	}
+	return data[index];
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('anime')
@@ -18,19 +40,18 @@ module.exports = {
 	 */
 	async execute(interaction) {
 		const desiredAnime = interaction.options.getString('query');
-		const query = new URLSearchParams({ query: desiredAnime });
+		const query = new URLSearchParams({ q: desiredAnime });
+		query.append('type', 'tv');
 
-		const animeSearch = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`).then((response) => response.json());
+		const animeSearch = await fetch(`https://api.jikan.moe/v4/anime?${query}`).then((response) => response.json());
+
 		if (!animeSearch.data.length) {
 			interaction.reply('Sorry, this anime does not seem to exist or it was incorrectly typed. Please try again.');
 		} else {
-			const animeResult =
-				animeSearch.data.find(
-					(anime) => anime.title.toLowerCase() === desiredAnime || anime.title.toLowerCase().includes(desiredAnime),
-				) ?? animeSearch.data[0];
-
+			const animeResult = findAnime(animeSearch.data, desiredAnime) ?? animeSearch.data[0];
+			
 			const imageUrl = animeResult.images.jpg.image_url;
-
+			
 			let episodeCount = 'N/A';
 			if (animeResult.episodes) {
 				episodeCount = animeResult.episodes.toString();
@@ -49,8 +70,8 @@ module.exports = {
 				.setURL(animeResult.url)
 				.setThumbnail(imageUrl)
 				.addFields(
-					{ name: 'Episode Count', value: episodeCount, inline: true },
 					{ name: 'Type', value: animeResult.type ?? 'N/A', inline: true },
+					{ name: 'Episode Count', value: episodeCount, inline: true },
 					{ name: 'Score', value: score, inline: true },
 					{ name: 'Synopsis', value: trimmedSynopsis, inline: false },
 				)
