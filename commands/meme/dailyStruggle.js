72ae2a49fcc66544ce10f5d30eca1494756e9adf
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-03 15:50:12 -0400

    Update dailyStruggle.js
    - fix for special characters
    - remove unused imports

--- commands/meme/dailyStruggle.js
+++ commands/meme/dailyStruggle.js
@@ -1,35 +1,33 @@
 const { EmbedBuilder, SlashCommandBuilder, quote, Client } = require('discord.js');
-
-const fetch = (...args) =>
-    import ('node-fetch').then(({ default: fetch }) => fetch(...args));
+const memeParser = require('../../utils/memeParser');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('daily_struggle')
-        .setDescription('Send a Daily Struggle meme with your desired text')
-        .addStringOption((option) =>
-            option.setName('left_button_text').setDescription('Text that appears on the left button').setRequired(true),
-        )
-        .addStringOption((option) =>
-            option.setName('right_button_text').setDescription('Text that appears on the right button').setRequired(true),
-        ),
-    guildOnly: true,
-    /**
-     * Sends a Daily Struggle meme with the specified text
-     * @param {*} interaction
-     */
-    async execute(interaction) {
-        const leftButton = interaction.options.getString('left_button_text');
-        const rightButton = interaction.options.getString('right_button_text');
+	data: new SlashCommandBuilder()
+		.setName('daily_struggle')
+		.setDescription('Send a Daily Struggle meme with your desired text')
+		.addStringOption((option) =>
+			option.setName('left_button_text').setDescription('Text that appears on the left button').setRequired(true),
+		)
+		.addStringOption((option) =>
+			option.setName('right_button_text').setDescription('Text that appears on the right button').setRequired(true),
+		),
+	guildOnly: true,
+	/**
+	 * Sends a Daily Struggle meme with the specified text
+	 * @param {*} interaction
+	 */
+	async execute(interaction) {
+		const leftButton = memeParser.parseText(interaction.options.getString('left_button_text'));
+		const rightButton = memeParser.parseText(interaction.options.getString('right_button_text'));
 
-        const struggleEmbed = new EmbedBuilder()
-            .setTitle('Daily Struggle Meme')
-            .setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
-            .setColor('#b35843')
-            .setTimestamp()
-            .setImage(`https://api.memegen.link/images/ds/${leftButton}/${rightButton}`)
-            .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const struggleEmbed = new EmbedBuilder()
+			.setTitle('Daily Struggle Meme')
+			.setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
+			.setColor(client.embedColour)
+			.setTimestamp()
+			.setImage(`https://api.memegen.link/images/ds/${leftButton}/${rightButton}`)
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 
-        interaction.reply({ embeds: [struggleEmbed] });
-    },
+		interaction.reply({ embeds: [struggleEmbed] });
+	},
 };
