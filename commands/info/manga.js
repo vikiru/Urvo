8f50feb62972cf31400033fabfaef51e10869a00
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-04 15:20:29 -0400

    Create manga.js
    - create manga info command

---
+++ commands/info/manga.js
@@ -0,0 +1,55 @@
+const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { URLSearchParams } = require('url');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('manga')
+		.setDescription('Sends the user information about a queried manga, obtained from MAL')
+		.addStringOption((option) =>
+			option.setName('query').setDescription('The name of the manga you wish to search for').setRequired(true),
+		),
+	guildOnly: true,
+	async execute(interaction) {
+		const desiredManga = interaction.options.getString('query');
+		const query = new URLSearchParams([
+			['q', `${desiredManga}`],
+			['type', 'manga'],
+		]);
+
+		const mangaSearch = await fetch(`https://api.jikan.moe/v4/manga?${query}`).then((response) => response.json());
+		const mangaResult = mangaSearch.data[0];
+
+		const imageUrl = mangaResult.images.jpg.image_url;
+
+		let chapterCount = '0';
+		if (mangaResult.chapterCount) {
+			chapterCount = mangaResult.chapterCount.toString();
+		}
+
+		let score = '0';
+		if (mangaResult.score) {
+			score = mangaResult.score.toString();
+		}
+
+		let synopsis = mangaResult.synopsis ?? 'N/A'
+		const trimmedSynopsis = trim(synopsis, 1024).toString();
+
+		const mangaEmbed = new EmbedBuilder()
+			.setTitle(mangaResult.title)
+			.setColor('#b35843')
+			.setTimestamp()
+			.setURL(mangaResult.url)
+			.setThumbnail(imageUrl)
+			.addFields(
+				{ name: 'Type', value: mangaResult.type ?? 'N/A', inline: true },
+				{ name: 'Chapter Count', value: chapterCount, inline: true },
+				{ name: 'Score', value: score, inline: true },
+				{ name: 'Synopsis', value: trimmedSynopsis, inline: false },
+			)
+			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+		interaction.reply({ embeds: [mangaEmbed] });
+	},
+};
