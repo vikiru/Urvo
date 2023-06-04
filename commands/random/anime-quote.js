e128af29fbbc02d731bb396d1aa32b80fe272726
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-04 17:38:14 -0400

    refactor: anime-quote (#23)

--- commands/random/anime-quote.js
+++ commands/random/anime-quote.js
@@ -1,7 +1,41 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
-const { URLSearchParams } = require('url');
-
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+/**
+ * Fetch data from the anime quote API and return the quote data object
+ * @returns An object containing quote information
+ */
+async function fetchData() {
+	try {
+		const quoteData = await fetch('https://animechan.vercel.app/api/random').then((response) => response.json());
+		return quoteData;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+/**
+ * Create the anime quote embed
+ * @param {*} interaction
+ * @param {*} quoteData
+ * @returns An embed containing all the information about the quote
+ */
+function createEmbed(interaction, quoteData) {
+	const animeQuote = quote('"' + quoteData.quote + '"');
+	const title = `💬 Random Quote from ${quoteData.anime}`;
+	const character = quoteData.character;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const quoteEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.addFields({ name: 'Quote', value: animeQuote }, { name: 'Character', value: character })
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return quoteEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('anime-quote').setDescription('Send a random quote from an anime'),
@@ -12,16 +46,10 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const quoteData = await fetch('https://animechan.vercel.app/api/random').then((response) => response.json());
-		const animeQuote = quote('"' + quoteData.quote + '"');
+		const quoteData = fetchData();
 
-		const quoteEmbed = new EmbedBuilder()
-			.setTitle(`💬 Random Quote from ${quoteData.anime}`)
+		const quoteEmbed = createEmbed(interaction, quoteData);
 
-			.setColor('#b35843')
-			.setTimestamp()
-			.addFields({ name: 'Quote', value: animeQuote }, { name: 'Character', value: quoteData.character })
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 		interaction.reply({ embeds: [quoteEmbed] });
 	},
 };
