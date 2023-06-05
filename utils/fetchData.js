d7ceaf649ac50a785f5dbbf9052b6cf8e60aafc2
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-05 13:02:53 -0400

    feat: add utility fetch function
    - create utility fetch data function

---
+++ utils/fetchData.js
@@ -0,0 +1,15 @@
+/**
+ * Fetch a given URL and return the response data as an object.
+ * @param {*} URL
+ * @returns JSON Response Data
+ */
+async function fetchData(URL) {
+	try {
+		const data = await fetch(URL).then((response) => response.json());
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+module.exports.fetchData = fetchData;
