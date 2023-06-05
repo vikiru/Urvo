d3227812517c13ef597d692fb3162eaf32bfb95d
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-05 13:11:52 -0400

    refactor: advice (#24)
    * refactor: advice
    
    * fix: anime-quote
    
    * feat: add utility fetch function
    
    - create utility fetch data function
    
    * fix: add missing import
    
    * fix: use utility fetchData function instead
    
    * minor: remove console.log

--- commands/random/advice.js
+++ commands/random/advice.js
@@ -1,6 +1,31 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Create the embed containing information about the random advice
+ * @param {*} interaction
+ * @param {*} randomAdvice
+ * @returns An embed containing information about the random advice
+ */
+function createEmbed(interaction, randomAdvice) {
+	const title = 'Random Advice!';
+	const description = randomAdvice.slip.advice;
+
+	const image = 'https://cdn.pixabay.com/photo/2013/07/12/14/48/dialog-148815_1280.png';
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const adviceEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setThumbnail(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return adviceEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('advice').setDescription('Send a random piece of advice'),
@@ -10,15 +35,9 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomAdvice = await fetch('https://api.adviceslip.com/advice').then((response) => response.json());
+		const randomAdvice = await fetchData('https://api.adviceslip.com/advice');
 
-		const adviceEmbed = new EmbedBuilder()
-			.setTitle(`Random Advice!`)
-			.setDescription(randomAdvice.slip.advice)
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage('https://cdn.pixabay.com/photo/2013/07/12/14/48/dialog-148815_1280.png')
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const adviceEmbed = createEmbed(interaction, randomAdvice);
 
 		interaction.reply({ embeds: [adviceEmbed] });
 	},
