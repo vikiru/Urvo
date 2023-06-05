4cb6008c99853b7c77dda1d45e770f8337e9c1f9
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-05 13:42:03 -0400

    refactor: bored (#29)
    * minor: remove fetchData from anime-quote
    
    * refactor: bored
    
    * minor: remove unused import and assign embed title to variable

--- commands/random/bored.js
+++ commands/random/bored.js
@@ -1,5 +1,39 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const { fetchData } = require('../../utils/fetchData');
+
+/**
+ * Create an embed containing information about the random activity
+ * @param {*} interaction
+ * @param {*} randomActivity
+ * @returns An embed containing information about the random activity
+ */
+function createEmbed(interaction, randomActivity) {
+	const title = 'Random Activity Suggestion!';
+	const activity = randomActivity.activity;
+	const type = randomActivity.type;
+	const participantCount = randomActivity.participants.toString();
+
+	const image = 'https://cdn.pixabay.com/photo/2015/05/29/19/18/bicycle-789648_1280.jpg';
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const activityEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setThumbnail(image)
+		.addFields(
+			{ name: 'Activity', value: activity, inline: true },
+			{ name: 'Type', value: type, inline: true },
+			{ name: 'Participants', value: participantCount, inline: true },
+		)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	if (randomActivity.link) {
+		activityEmbed.setURL(randomActivity.link);
+	}
+	return activityEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('bored').setDescription('Suggest a random randomActivity to the user'),
@@ -9,25 +43,9 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomActivity = await fetch('http://www.boredapi.com/api/activity/').then((response) => response.json());
+		const randomActivity = await fetchData('http://www.boredapi.com/api/activity/');
 
-		const activityEmbed = new EmbedBuilder()
-			.setTitle('Random Activity Suggestion!')
-			.setColor('#b35843')
-			.setTimestamp()
-			.setThumbnail(
-				'https://res.cloudinary.com/teepublic/image/private/s--Zs-fk3E1--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_484849,e_outline:48/co_484849,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1499630965/production/designs/1725535_0.jpg',
-			)
-			.addFields(
-				{ name: 'Activity', value: randomActivity.activity, inline: true },
-				{ name: 'Type', value: randomActivity.type, inline: true },
-				{ name: 'Participants', value: randomActivity.participants.toString(), inline: true },
-			)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
-		if (randomActivity.link) {
-			activityEmbed.setURL(randomActivity.link);
-		}
+		const activityEmbed = createEmbed(interaction, randomActivity);
 
 		interaction.reply({ embeds: [activityEmbed] });
 	},
