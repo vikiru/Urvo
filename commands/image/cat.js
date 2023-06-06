3ffae36c8b97dd37047a02ce6b2f0c9c478ccea6
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-06 18:23:24 -0400

    refactor: cat

--- commands/image/cat.js
+++ commands/image/cat.js
@@ -1,7 +1,30 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData.js');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Creates an embed containing information about a random cat
+ * @param {*} interaction
+ * @param {*} randomCat
+ * @returns An embed containing information about a random cat
+ */
+function createEmbed(interaction, randomCat) {
+	const title = '🙀 Random Cat!';
 
+	const image = randomCat.image;
+	const fact = quote(randomCat.fact);
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const catEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(fact)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return catEmbed;
+}
 module.exports = {
 	data: new SlashCommandBuilder().setName('cat').setDescription('Send a random image of a cat'),
 	guildOnly: true,
@@ -10,15 +33,8 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomCat = await fetch('https://some-random-api.com/animal/cat').then((response) => response.json());
-
-		const catEmbed = new EmbedBuilder()
-			.setTitle('🙀 Random Cat!')
-			.setDescription(quote(randomCat.fact))
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage(randomCat.image)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const randomCat = await fetchData('https://some-random-api.com/animal/cat');
+		const catEmbed = createEmbed(interaction, randomCat);
 		interaction.reply({ embeds: [catEmbed] });
 	},
 };
