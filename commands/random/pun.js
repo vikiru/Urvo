d23f672bc992fdebcb183fc554281f0230dcded6
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-06 17:52:50 -0400

    refactor: pun (#39)

--- commands/random/pun.js
+++ commands/random/pun.js
@@ -1,6 +1,30 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData.js');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Create an embed containing information about the random pun
+ * @param {*} interaction
+ * @param {*} randomPun
+ * @returns An embed containing information about the random pun
+ */
+function createEmbed(interaction, randomPun) {
+	const title = 'Random Pun!';
+	const image = 'https://cdn.pixabay.com/photo/2016/10/26/16/39/paddington-1771898_1280.jpg';
+
+	const joke = randomPun.joke;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const punEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(joke)
+		.setColor(client.embedColour)
+		.setTimestamp(image)
+		.setImage()
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return punEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('pun').setDescription('Send a random pun'),
@@ -10,17 +34,10 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomPun = await fetch(
+		const randomPun = await fetchData(
 			'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single',
-		).then((response) => response.json());
-
-		const punEmbed = new EmbedBuilder()
-			.setTitle(`Random Pun!`)
-			.setDescription(randomPun.joke)
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage('https://cdn.pixabay.com/photo/2016/10/26/16/39/paddington-1771898_1280.jpg')
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		);
+		const punEmbed = createEmbed(interaction, randomPun);
 
 		interaction.reply({ embeds: [punEmbed] });
 	},
