f69b4920405eb3baea7a8a20c85505687c1af95f
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-06 18:14:51 -0400

    refactor: throne (#45)

--- commands/random/thrones.js
+++ commands/random/thrones.js
@@ -1,6 +1,41 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData.js');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Fetch info about a random character from Game of Thrones via the Thrones API
+ * @returns An object containing the character information
+ */
+async function getRandomThrone() {
+	const thronesData = await fetchData('https://thronesapi.com/api/v2/Characters');
+	const maxNum = thronesData.length ?? 53;
+	const random = Math.floor(Math.random() * maxNum);
+	return thronesData[random];
+}
+
+/**
+ * Create an embed containing information about the random Game of Thrones character
+ * @param {*} interaction
+ * @param {*} randomThrone
+ * @returns An embed containing information about the random Game of Thrones character
+ */
+function createEmbed(interaction, randomThrone) {
+	const name = randomThrone.fullName;
+	const image = randomThrone.imageUrl;
+	const title = randomThrone.title;
+	const family = randomThrone.family;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.displayAvatarURL();
+
+	const throneEmbed = new EmbedBuilder()
+		.setTitle(name)
+		.setColor(client.embedColour)
+		.setImage(image)
+		.setTimestamp()
+		.addFields({ name: 'Title', value: title, inline: true }, { name: 'family', value: family, inline: true })
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return throneEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,25 +47,8 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const thronesData = await fetch('https://thronesapi.com/api/v2/Characters').then((response) => response.json());
-		const maxNum = thronesData.length ?? 53;
-		const random = Math.floor(Math.random() * maxNum);
-
-		const randomThrone = await fetch(`https://thronesapi.com/api/v2/Characters/${random}`).then((response) =>
-			response.json(),
-		);
-
-		const throneEmbed = new EmbedBuilder()
-			.setTitle(randomThrone.fullName)
-			.setColor('#b35843')
-			.setImage(randomThrone.imageUrl)
-			.setTimestamp()
-			.addFields(
-				{ name: 'Title', value: randomThrone.title, inline: true },
-				{ name: 'family', value: randomThrone.family, inline: true },
-			)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		const randomThrone = await getRandomThrone();
+		const throneEmbed = createEmbed(interaction, randomThrone);
 		interaction.reply({ embeds: [throneEmbed] });
 	},
 };
