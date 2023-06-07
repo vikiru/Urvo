33dbc79f6990a63735fde224dc286b42667ede86
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-07 18:21:20 -0400

    refactor: koala

--- commands/image/koala.js
+++ commands/image/koala.js
@@ -1,6 +1,29 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Create an embed containing information about the random koala
+ * @param {*} interaction
+ * @param {*} randomKoala
+ * @returns An embed containing information about the random koala
+ */
+function createEmbed(interaction, randomKoala) {
+	const title = '🐨 Random Koala!';
+	const description = quote(randomKoala.fact);
+	const image = randomKoala.image;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const koalaEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return koalaEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('koala').setDescription('Send a random image of a koala'),
@@ -10,15 +33,8 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomKoala = await fetch('https://some-random-api.com/animal/koala').then((response) => response.json());
-
-		const koalaEmbed = new EmbedBuilder()
-			.setTitle('🐨 Random Koala!')
-			.setDescription(quote(randomKoala.fact))
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage(randomKoala.image)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const randomKoala = await fetchData('https://some-random-api.com/animal/koala');
+		const koalaEmbed = createEmbed(interaction, randomKoala);
 		interaction.reply({ embeds: [koalaEmbed] });
 	},
 };
