200861fa4b9c206bcfde89a70c167d980e03b216
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-07 18:30:46 -0400

    refactor: red-panda (#80)

--- commands/image/redPanda.js
+++ commands/image/redPanda.js
@@ -1,6 +1,29 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Create an embed containing information about the random red panda
+ * @param {*} interaction
+ * @param {*} randomRedPanda
+ * @returns An embed containing information about the random red panda
+ */
+function createEmbed(interaction, randomRedPanda) {
+	const title = 'Random Red Panda!';
+	const description = quote(randomRedPanda.fact);
+	const image = randomRedPanda.image;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const redPandaEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return redPandaEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('red_panda').setDescription('Send a random image of a red panda'),
@@ -10,17 +33,8 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomRedPanda = await fetch('https://some-random-api.com/animal/red_panda').then((response) =>
-			response.json(),
-		);
-
-		const redPandaEmbed = new EmbedBuilder()
-			.setTitle('Random Red Panda!')
-			.setDescription(quote(randomRedPanda.fact))
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage(randomRedPanda.image)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const randomRedPanda = await fetchData('https://some-random-api.com/animal/red_panda');
+		const redPandaEmbed = createEmbed(interaction, randomRedPanda);
 		interaction.reply({ embeds: [redPandaEmbed] });
 	},
 };
