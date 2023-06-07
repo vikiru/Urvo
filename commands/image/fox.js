aabe3c748a12b1395bf6544a5130aa3ee927b985
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-07 18:14:28 -0400

    refactor: fox (#75)

--- commands/image/fox.js
+++ commands/image/fox.js
@@ -1,6 +1,30 @@
 const { EmbedBuilder, SlashCommandBuilder, quote } = require('discord.js');
+const { fetchData } = require('../../utils/fetchData');
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/**
+ * Create an embed containing information about a random fox
+ * @param {*} interaction
+ * @param {*} randomFox
+ * @returns An embed containing information about the random fox
+ */
+function createEmbed(interaction, randomFox) {
+	const title = '🦊 Random Fox!';
+	const description = quote(randomFox.fact);
+	const image = randomFox.image;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const foxEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	interaction.reply({ embeds: [foxEmbed] });
+	return foxEmbed;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('fox').setDescription('Send a random picture of a fox'),
@@ -10,15 +34,8 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const randomFox = await fetch('https://some-random-api.com/animal/fox').then((response) => response.json());
-
-		const foxEmbed = new EmbedBuilder()
-			.setTitle('🦊 Random Fox!')
-			.setDescription(quote(randomFox.fact))
-			.setColor('#b35843')
-			.setTimestamp()
-			.setImage(randomFox.image)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const randomFox = await fetchData('https://some-random-api.com/animal/fox');
+		const foxEmbed = createEmbed(interaction, randomFox);
 		interaction.reply({ embeds: [foxEmbed] });
 	},
 };
