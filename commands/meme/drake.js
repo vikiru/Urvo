7057e71382928b54cd2edf09efd0b56816d04103
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-13 15:53:31 -0400

    refactor: drake (#95)

--- commands/meme/drake.js
+++ commands/meme/drake.js
@@ -1,6 +1,38 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const memeParser = require('../../utils/memeParser');
 
+/**
+ * Create an embed containing information about the Drake Meme
+ * @param {*} interaction
+ * @param {*} options
+ * @returns An embed containing information about the Drake Meme
+ */
+function createEmbed(interaction, options) {
+	const title = 'Drake Hotline Bling Meme';
+	const description =
+		'Here is your requested meme! \n\nGenerated via' +
+		hyperlink({ content: 'Memegen.link', url: 'https://memegen.link/' });
+
+	const topText = options.topText;
+	const bottomText = options.bottomText;
+	const format = options.format;
+
+	const image = `https://api.memegen.link/images/drake/${topText}/${bottomText}${format}`;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const drakeEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return drakeEmbed;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('drake')
@@ -19,15 +51,9 @@ module.exports = {
 	async execute(interaction) {
 		const topText = memeParser.parseText(interaction.options.getString('top_text'));
 		const bottomText = memeParser.parseText(interaction.options.getString('bottom_text'));
+		const options = { topText: topText, bottomText: bottomText, format: '.png' };
 
-		const drakeEmbed = new EmbedBuilder()
-			.setTitle('Drake Hotline Bling Meme')
-			.setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
-			.setColor(client.embedColour)
-			.setTimestamp()
-			.setImage(`https://api.memegen.link/images/drake/${topText}/${bottomText}.png`)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		const drakeEmbed = createEmbed(interaction, options);
 		interaction.reply({ embeds: [drakeEmbed] });
 	},
 };
