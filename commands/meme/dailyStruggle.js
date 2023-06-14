ca6598f2caa1a370580408f51abca48d3324b8cf
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-13 20:25:48 -0400

    refactor: daily-struggle

--- commands/meme/dailyStruggle.js
+++ commands/meme/dailyStruggle.js
@@ -1,6 +1,38 @@
 const { EmbedBuilder, SlashCommandBuilder, hyperlink } = require('discord.js');
 const memeParser = require('../../utils/memeParser');
 
+/**
+ * Create an embed containing information about the Daily Struggle Meme
+ * @param {*} interaction
+ * @param {*} options
+ * @returns An embed containing information about the Daily Struggle Meme
+ */
+function createEmbed(interaction, options) {
+	const title = 'Daily Struggle Meme';
+	const description =
+		'Here is your requested meme! \n\nGenerated via' +
+		hyperlink({ content: 'Memegen.link', url: 'https://memegen.link/' });
+
+	const leftButton = options.leftButton;
+	const rightButton = options.rightButton;
+	const format = options.format;
+
+	const image = `https://api.memegen.link/images/ds/${leftButton}/${rightButton}${format}`;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const struggleEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return struggleEmbed;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('daily_struggle')
@@ -20,14 +52,8 @@ module.exports = {
 		const leftButton = memeParser.parseText(interaction.options.getString('left_button_text'));
 		const rightButton = memeParser.parseText(interaction.options.getString('right_button_text'));
 
-		const struggleEmbed = new EmbedBuilder()
-			.setTitle('Daily Struggle Meme')
-			.setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
-			.setColor(client.embedColour)
-			.setTimestamp()
-			.setImage(`https://api.memegen.link/images/ds/${leftButton}/${rightButton}`)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		const options = { leftButton: leftButton, rightButton: rightButton, format: '.png' };
+		const struggleEmbed = createEmbed(interaction, options);
 		interaction.reply({ embeds: [struggleEmbed] });
 	},
 };
