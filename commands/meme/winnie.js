fdc55ebf3019e07b57a24016cf91a7a9b9c5d89b
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-13 20:04:03 -0400

    refactor: winnie (#99)

--- commands/meme/winnie.js
+++ commands/meme/winnie.js
@@ -1,6 +1,38 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const memeParser = require('../../utils/memeParser');
 
+/**
+ * Create an embed containing information about the Tuxedo Winnie the Pooh Meme
+ * @param {*} interaction
+ * @param {*} options
+ * @returns An embed containing information about the Tuxedo Winnie the Pooh Meme
+ */
+function createEmbed(interaction, options) {
+	const title = 'Tuxedo Winnie the Pooh Meme';
+	const description =
+		'Here is your requested meme! \n\nGenerated via' +
+		hyperlink({ content: 'Memegen.link', url: 'https://memegen.link/' });
+
+	const topText = options.topText;
+	const bottomText = options.bottomText;
+	const format = options.format;
+
+	const image = `https://api.memegen.link/images/pooh/${topText}/${bottomText}${format}`;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const mindEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return mindEmbed;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('winnie')
@@ -19,15 +51,9 @@ module.exports = {
 	async execute(interaction) {
 		const topText = memeParser.parseText(interaction.options.getString('top_text'));
 		const bottomText = memeParser.parseText(interaction.options.getString('bottom_text'));
-
-		const winnieEmbed = new EmbedBuilder()
-			.setTitle('Tuxedo Winnie the Pooh Meme')
-			.setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
-			.setColor(client.embedColour)
-			.setTimestamp()
-			.setImage(`https://api.memegen.link/images/pooh/${topText}/${bottomText}`)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		
+		const options = { topText: topText, bottomText: bottomText, format: '.png' };
+		const winnieEmbed = createEmbed(interaction, options);
 		interaction.reply({ embeds: [winnieEmbed] });
 	},
 };
