9b9c72ea45cb2fc4c9d4accf6f4f44fc3f99a546
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-13 20:16:48 -0400

    refactor: knuckles

--- commands/meme/knuckles.js
+++ commands/meme/knuckles.js
@@ -1,6 +1,38 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const memeParser = require('../../utils/memeParser');
 
+/**
+ * Create an embed containing information about the Ugandan Knuckles Meme
+ * @param {*} interaction
+ * @param {*} options
+ * @returns An embed containing information about the Ugandan Knuckles Meme
+ */
+function createEmbed(interaction, options) {
+	const title = 'Ugandan Knuckles Meme';
+	const description =
+		'Here is your requested meme! \n\nGenerated via' +
+		hyperlink({ content: 'Memegen.link', url: 'https://memegen.link/' });
+
+	const topText = options.topText;
+	const bottomText = options.bottomText;
+	const format = options.format;
+
+	const image = `https://api.memegen.link/images/ugandanknuck/${topText}/${bottomText}${format}`;
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarUrl();
+
+	const knuckleEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setImage(image)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return knuckleEmbed;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('knuckles')
@@ -20,14 +52,8 @@ module.exports = {
 		const topText = memeParser.parseText(interaction.options.getString('top_text'));
 		const bottomText = memeParser.parseText(interaction.options.getString('bottom_text'));
 
-		const knucklesEmbed = new EmbedBuilder()
-			.setTitle('Ugandan Knuckles Meme')
-			.setDescription(`Here is your requested meme! \n\n[Generated via Memegen.link](https://memegen.link/)`)
-			.setColor(client.embedColour)
-			.setTimestamp()
-			.setImage(`https://api.memegen.link/images/ugandanknuck/${topText}/${bottomText}.png`)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		const options = { topText: topText, bottomText: bottomText, format: '.png' };
+		const knucklesEmbed = createEmbed(interaction, options);
 		interaction.reply({ embeds: [knucklesEmbed] });
 	},
 };
