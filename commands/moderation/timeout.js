b6d9c3dad0f519e98fc62ca03c31eb49cb20bc3e
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-17 14:34:16 -0400

    refactor: timeout

--- commands/moderation/timeout.js
+++ commands/moderation/timeout.js
@@ -5,22 +5,7 @@ const {
 	ActionRowBuilder,
 	ButtonBuilder,
 } = require('discord.js');
-
-/**
- * Returns the timeout value in milliseconds for a given duration in a specified unit of time.
- * @param {*} duration
- * @param {*} units
- * @returns Timeout duration in milliseconds
- */
-function returnTimeVal(duration, units) {
-	if (units === 'minutes') {
-		return duration * 60 * 1000;
-	} else if (units === 'hours') {
-		return duration * 60 * 60 * 1000;
-	} else if (units === 'days') {
-		return duration * 24 * 60 * 60 * 1000;
-	}
-}
+const ms = require('ms');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -30,17 +15,17 @@ module.exports = {
 			option.setName('user').setDescription('The user to timeout from the server').setRequired(true),
 		)
 		.addNumberOption((option) =>
-			option.setName('duration').setDescription('How long the user should be timed out').setRequired(true),
-		)
-		.addStringOption((option) =>
 			option
-				.setName('units')
-				.setDescription('Specify what unit of time the duration of the timeout is')
+				.setName('duration')
+				.setDescription('How long the user should be timed out')
 				.setRequired(true)
 				.addChoices(
-					{ name: 'Minutes', value: 'minutes' },
-					{ name: 'Hours', value: 'hours' },
-					{ name: 'Days', value: 'days' },
+					{ name: '60 Seconds', value: 60_000 },
+					{ name: '5 Minutes', value: 300_000 },
+					{ name: '10 Minutes', value: 600_000 },
+					{ name: '1 Hour', value: 3_600_000 },
+					{ name: '1 Day', value: 86_400_000 },
+					{ name: '1 Week', value: 604_800_000 },
 				),
 		)
 		.addStringOption((option) =>
@@ -53,19 +38,17 @@ module.exports = {
 	 * @param {*} interaction
 	 */
 	async execute(interaction) {
-		const targetUser = interaction.options.getUser('user');
+		const targetUser = interaction.options.getMember('user');
 		const duration = interaction.options.getNumber('duration');
-		const units = interaction.options.getString('units');
+		const durationString = ms(duration, { long: true });
 		const reason = interaction.options.getString('reason') ?? 'No reason provided';
-
-		const miliDuration = returnTimeVal(duration, units);
 
 		const confirmTimeout = new ButtonBuilder().setCustomId('confirm').setLabel('Confirm').setStyle(ButtonStyle.Danger);
 		const cancelTimeout = new ButtonBuilder().setCustomId('cancel').setLabel('Cancel').setStyle(ButtonStyle.Secondary);
 		const row = new ActionRowBuilder().addComponents(cancelTimeout, confirmTimeout);
 
 		const response = await interaction.reply({
-			content: `Are you sure you want to timeout <@${targetUser.id}> from the server?`,
+			content: `Are you sure you want to timeout <@${targetUser.id}> for ${durationString}?`,
 			components: [row],
 		});
 		const filter = (i) => i.user.id === interaction.user.id;
@@ -75,9 +58,9 @@ module.exports = {
 			const confirmId = confirmation.customId;
 
 			if (confirmId === 'confirm') {
-				await targetUser.timeout(miliDuration, reason);
+				await targetUser.timeout(duration, reason);
 				await interaction.editReply({
-					content: `Successfully timed out <@${targetUser.id}> for ${reason}. As requested by <@${interaction.user.id}>`,
+					content: `Successfully timed out <@${targetUser.id}> for reason: ${reason} and duration: ${durationString}. As requested by <@${interaction.user.id}>`,
 					components: [],
 				});
 			} else if (confirmId === 'cancel') {
@@ -91,6 +74,7 @@ module.exports = {
 				content: `Confirmation to timeout <@${targetUser.id}> was not received within 1 minute. Aborting command execution.`,
 				components: [],
 			});
+			console.log(error);
 		}
 	},
 };
