295b06d6b5f51ec107e8a1f2ce817fb6203bdfc6
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-19 15:32:49 -0400

    refactor: top-anime

--- commands/info/topAnime.js
+++ commands/info/topAnime.js
@@ -1,7 +1,12 @@
 const { EmbedBuilder, SlashCommandBuilder, codeBlock, inlineCode, hyperlink } = require('discord.js');
 const { URLSearchParams } = require('url');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const { fetchData } = require('../../utils/fetchData');
 
+/**
+ * Format each anime within the top 10 and display them as a field within the created embed
+ * @param {*} data
+ * @returns A string representing the formatted data of each anime
+ */
 function formatAnime(data) {
 	const title = data.title.replaceAll('"', '');
 	const malURL = data.url;
@@ -10,10 +15,10 @@ function formatAnime(data) {
 	let episodeCount = data.episodes;
 
 	if (episodeCount === null) {
-		episodeCount = 0;
+		episodeCount = 'NA';
 	}
 	if (score === null) {
-		score = 0;
+		score = 'NA';
 	}
 
 	let formattedString = codeBlock(title) + '\n';
@@ -28,6 +33,40 @@ function formatAnime(data) {
 	formattedString += inlineCode('Score') + ': ' + score + '\n';
 	formattedString += inlineCode('Episodes') + ': ' + episodeCount;
 	return formattedString;
+}
+
+/**
+ * Create an embed containing information about the top 10 anime (airing or all time)
+ * @param {*} interaction
+ * @param {*} status
+ * @param {*} topAnimeData
+ * @returns An embed containing information about the top 10 anime (airing or all time)
+ */
+function createEmbed(interaction, status, topAnimeData) {
+	const title = status === 'airing' ? 'Airing' : 'All Time';
+	console.log(interaction.user);
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const topAnimeEmbed = new EmbedBuilder()
+		.setTitle(`Top 10 ${title} Anime!`)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.addFields(
+			{ name: '1.', value: formatAnime(topAnimeData[0]), inline: true },
+			{ name: '2.', value: formatAnime(topAnimeData[1]), inline: true },
+			{ name: '3.', value: formatAnime(topAnimeData[2]), inline: true },
+			{ name: '4.', value: formatAnime(topAnimeData[3]), inline: true },
+			{ name: '5.', value: formatAnime(topAnimeData[4]), inline: true },
+			{ name: '6.', value: formatAnime(topAnimeData[5]), inline: true },
+			{ name: '7.', value: formatAnime(topAnimeData[6]), inline: true },
+			{ name: '8.', value: formatAnime(topAnimeData[7]), inline: true },
+			{ name: '9.', value: formatAnime(topAnimeData[8]), inline: true },
+			{ name: '10.', value: formatAnime(topAnimeData[9]), inline: true },
+		)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+
+	return topAnimeEmbed;
 }
 
 module.exports = {
@@ -49,28 +88,9 @@ module.exports = {
 			['type', 'tv'],
 			['limit', limit],
 		]);
-
-		const results = await fetch(`https://api.jikan.moe/v4/top/anime?${query}`).then((response) => response.json());
+		const results = await fetchData(`https://api.jikan.moe/v4/top/anime?${query}`);
 		const topAnime = results.data;
-
-		const titleStatusText = status === 'airing' ? 'Airing' : 'All Time';
-		const topAnimeEmbed = new EmbedBuilder()
-			.setTitle(`Top 10 ${titleStatusText} Anime!`)
-			.setColor(client.embedColour)
-			.setTimestamp()
-			.addFields(
-				{ name: '1.', value: formatAnime(topAnime[0]), inline: true },
-				{ name: '2.', value: formatAnime(topAnime[1]), inline: true },
-				{ name: '3.', value: formatAnime(topAnime[2]), inline: true },
-				{ name: '4.', value: formatAnime(topAnime[3]), inline: true },
-				{ name: '5.', value: formatAnime(topAnime[4]), inline: true },
-				{ name: '6.', value: formatAnime(topAnime[5]), inline: true },
-				{ name: '7.', value: formatAnime(topAnime[6]), inline: true },
-				{ name: '8.', value: formatAnime(topAnime[7]), inline: true },
-				{ name: '9.', value: formatAnime(topAnime[8]), inline: true },
-				{ name: '10.', value: formatAnime(topAnime[9]), inline: true },
-			)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+		const topAnimeEmbed = createEmbed(interaction, status, topAnime);
 		interaction.reply({ embeds: [topAnimeEmbed] });
 	},
 };
