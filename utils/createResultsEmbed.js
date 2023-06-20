9ea0b4aa691e4cb40bb516e929fa758b3c0a72ed
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-06-20 18:59:35 -0400

    refactor: anime (#121)

---
+++ utils/createResultsEmbed.js
@@ -0,0 +1,37 @@
+const { EmbedBuilder, inlineCode, hyperlink } = require('discord.js');
+
+/**
+ * Create a results embed with all of the potential anime, manga, manhua or manhwa corresponding to a given search query
+ * @param {*} interaction
+ * @param {*} indexes
+ * @param {*} title
+ * @param {*} type
+ * @returns An embed with all of the potential anime, manga, manhua or manhwa corresponding to a given search query
+ */
+function createResultsEmbed(interaction, indexes, title, type) {
+	let description = `Please choose an ${type} from the following list of ${inlineCode(
+		indexes.length,
+	)} ${type}, to see more information regarding it.\n`;
+
+	for (let i = 0; i < indexes.length; i++) {
+		const index = indexes[i];
+		const indexTitle = index.title;
+		const url = index.url;
+		const count = i + 1;
+
+		description += `${count}. ${hyperlink(indexTitle, url)}\n`;
+	}
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const resultsEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(description)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return resultsEmbed;
+}
+
+module.exports.createResultsEmbed = createResultsEmbed;
---
+++ utils/findEntry.js
@@ -0,0 +1,27 @@
+/**
+ * Iterate through all the MAL search results to find entries that match the specified title and type
+ * @param {*} data
+ * @param {*} title
+ * @param {*} type
+ * @returns A filtered array of MAL search results that match the specified title and type
+ */
+function findEntry(data, title, type) {
+	const indexes = [];
+	for (const element of data) {
+		const entryType = element.type;
+		if (entryType === type) {
+			const titles = element.titles.map((titleEntry) => titleEntry.title.toLowerCase());
+			const titleMatchExists = titles.findIndex(
+				(titleEntry) => titleEntry.toLowerCase() === title || titleEntry.toLowerCase().includes(title),
+			);
+			const titleIncluded = titles.includes(title.toLowerCase());
+			if (titleMatchExists || titleIncluded) {
+				indexes.push(element);
+			}
+		}
+	}
+	const sortedIndexes = indexes.slice(0, 9).sort((a, b) => a.year - b.year);
+	return sortedIndexes;
+}
+
+module.exports.findEntry = findEntry;
