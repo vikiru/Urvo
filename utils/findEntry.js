0af86f757fc8364afe9ff612486f0d4836d0880d
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-20 20:06:49 -0400

    fix: findEntry now properly works and limits 10 entries

--- utils/findEntry.js
+++ utils/findEntry.js
@@ -7,13 +7,12 @@
  */
 function findEntry(data, title, type) {
 	const indexes = [];
+	console.log(data);
 	for (const element of data) {
 		const entryType = element.type;
 		if (entryType === type) {
 			const titles = element.titles.map((titleEntry) => titleEntry.title.toLowerCase());
-			const titleMatchExists = titles.findIndex(
-				(titleEntry) => titleEntry.toLowerCase() === title || titleEntry.toLowerCase().includes(title),
-			);
+			const titleMatchExists = titles.find((titleEntry) => titleEntry === title || titleEntry.includes(title));
 			const titleIncluded = titles.includes(title.toLowerCase());
 			if (titleMatchExists || titleIncluded) {
 				indexes.push(element);
@@ -21,7 +20,7 @@ function findEntry(data, title, type) {
 		}
 	}
 	let sortedIndexes = indexes.slice().sort((a, b) => a.year - b.year);
-	sortedIndexes = sortedIndexes.slice(0, 9);
+	sortedIndexes = sortedIndexes.slice(0, 10);
 	return sortedIndexes;
 }
 
