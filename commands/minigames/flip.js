f989d5c0d91ceecb6dd1ce68f2d0e8c17901dbc1
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-26 17:27:27 -0400

    refactor: flip

--- commands/minigames/flip.js
+++ commands/minigames/flip.js
@@ -9,6 +9,36 @@ function properCase(str) {
 	return str;
 }
 
+/**
+ * Create an embed containing information about the result of the coin flip
+ * @param {*} interaction
+ * @param {*} data
+ * @returns An embed containing information about the result of the coin flip
+ */
+function createEmbed(interaction, data) {
+	const title = 'Coin Flip!';
+	const choice = data.choice;
+	const outcome = data.outcome;
+	const result = data.result;
+	const img = 'https://karenstrunks.com/wp-content/uploads/2014/06/HEADS-TAILS.jpg';
+
+	const username = interaction.user.username;
+	const avatarURL = interaction.user.displayAvatarURL();
+
+	const resultEmbed = new EmbedBuilder()
+		.setTitle(title)
+		.setColor(client.embedColour)
+		.setTimestamp()
+		.setThumbnail(img)
+		.addFields(
+			{ name: `${username}'s Choice`, value: properCase(choice), inline: true },
+			{ name: 'Result', value: properCase(result), inline: true },
+			{ name: 'Outcome', value: outcome },
+		)
+		.setFooter({ text: `Requested by ${username}`, iconURL: avatarURL });
+	return resultEmbed;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('flip')
@@ -34,22 +64,10 @@ module.exports = {
 	async execute(interaction) {
 		const options = ['heads', 'tails'];
 		const result = options[Math.floor(Math.random() * options.length)];
-
 		const choice = interaction.options.getString('choice');
 		const outcome = result === choice ? 'You win!' : 'You lose!';
-
-		const resultEmbed = new EmbedBuilder()
-			.setTitle('Coin Flip!')
-			.setColor('#b35843')
-			.setTimestamp()
-			.setThumbnail('https://karenstrunks.com/wp-content/uploads/2014/06/HEADS-TAILS.jpg')
-			.addFields(
-				{ name: `${interaction.user.username}'s Choice`, value: properCase(choice), inline: true },
-				{ name: 'Result', value: properCase(result), inline: true },
-				{ name: 'Outcome', value: outcome },
-			)
-			.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
-
+		const data = { choice: choice, result: result, outcome: outcome };
+		const resultEmbed = createEmbed(interaction, data);
 		interaction.reply({ embeds: [resultEmbed] });
 	},
 };
