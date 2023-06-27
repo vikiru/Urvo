ea9579b34f6634b004433550ec40287aa36954a4
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-27 15:45:50 -0400

    minor: cleanup ready

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
+const { Events } = require('discord.js');
+
 module.exports = {
-	name: 'ready',
+	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} has logged in`);
-		client.user.setPresence({ activities: [{ name: 'Evolving...' }], status: 'online' });
+		client.user.setPresence({ activities: [{ name: '/help' }], status: 'online' });
 	},
 };
