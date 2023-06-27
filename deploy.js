62a67e403ab5e28fb4ce8051172e9bf0958254a5
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-27 15:40:48 -0400

    fix: fix deploy command script to work globally instead of per guild

--- deploy.js
+++ deploy.js
@@ -33,7 +33,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(Routes.applicationGuildCommands(clientId), { body: commands });
+		const data = await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
