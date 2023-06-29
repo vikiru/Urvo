da78ced80b5ddbdbd9983513b6e9cec73bba58bf
vikiru <72267229+vikiru@users.noreply.github.com>
2023-06-29 15:39:12 -0400

    docs: update sidebar with remaining sections

--- urvo-docs/sidebars.js
+++ urvo-docs/sidebars.js
@@ -1,14 +1,3 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
-
 // @ts-check
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
@@ -23,22 +12,124 @@ const sidebars = {
 		{
 			type: 'category',
 			label: 'Commands',
-			items: ['commands/category-overview'],
+			items: [
+				'commands/category-overview',
+				{
+					type: 'category',
+					label: '📷 Images',
+					items: [
+						'commands/images/bird',
+						'commands/images/cat',
+						'commands/images/coffee',
+						'commands/images/dog',
+						'commands/images/duck',
+						'commands/images/kangaroo',
+						'commands/images/koala',
+						'commands/images/panda',
+						'commands/images/raccoon',
+						'commands/images/red-panda',
+						'commands/images/whale',
+					],
+				},
+				{
+					type: 'category',
+					label: '❓ Information',
+					items: [
+						'commands/info/anime',
+						'commands/info/digimon',
+						'commands/info/disney',
+						'commands/info/guess-user',
+						'commands/info/manga',
+						'commands/info/manhua',
+						'commands/info/manhwa',
+						'commands/info/minecraft',
+						'commands/info/top-anime',
+						'commands/info/top-manga',
+						'commands/info/urban',
+					],
+				},
+				{
+					type: 'category',
+					label: '😂 Memes',
+					items: [
+						'commands/memes/buzz',
+						'commands/memes/change-my-mind',
+						'commands/memes/daily-struggle',
+						'commands/memes/drake',
+						'commands/memes/kermit',
+						'commands/memes/knuckles',
+						'commands/memes/mordor',
+						'commands/memes/philosoraptor',
+						'commands/memes/spongebob',
+						'commands/memes/winnie',
+					],
+				},
+				{
+					type: 'category',
+					label: '🕹️ Minigames',
+					items: ['commands/minigames/flip', 'commands/minigames/guess-number', 'commands/minigames/rps'],
+				},
+				{
+					type: 'category',
+					label: '🛡️ Moderation',
+					items: [
+						'commands/moderation/ban',
+						'commands/moderation/kick',
+						'commands/moderation/create-role',
+						'commands/moderation/timeout',
+						'commands/moderation/unban',
+					],
+				},
+				{
+					type: 'category',
+					label: '🎲 Random',
+					items: [
+						'commands/random/advice',
+						'commands/random/affirmation',
+						'commands/random/anime-quote',
+						'commands/random/bored',
+						'commands/random/comic',
+						'commands/random/fact',
+						'commands/random/harry-potter',
+						'commands/random/joke',
+						'commands/random/programming-joke',
+						'commands/random/pun',
+						'commands/random/quote',
+						'commands/random/shakespeare',
+						'commands/random/thrones',
+					],
+				},
+				{
+					type: 'category',
+					label: '⚔️ Troopica',
+					items: [
+						'commands/troopica/attack',
+						'commands/troopica/balance',
+						'commands/troopica/daily',
+						'commands/troopica/leaderboard',
+						'commands/troopica/purchase',
+					],
+				},
+				{
+					type: 'category',
+					label: '🛠️ Utility',
+					items: [
+						'commands/utility/calculate',
+						'commands/utility/create-channel',
+						'commands/utility/delete-channel',
+						'commands/utility/help',
+						'commands/utility/prune',
+						'commands/utility/server-info',
+					],
+				},
+			],
+		},
+		{
+			type: 'category',
+			label: 'Conclusion',
+			items: ['acknowledgements', 'commands/api-references'],
 		},
 	],
-
-	// But you can create a sidebar manually
-	/*
-  tutorialSidebar: [
-    'intro',
-    'hello',
-    {
-      type: 'category',
-      label: 'Tutorial',
-      items: ['tutorial-basics/create-a-document'],
-    },
-  ],
-   */
 };
 
 module.exports = sidebars;
