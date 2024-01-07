62dc231d8a31180bb32066a734332394072f021f
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-07 11:38:49 -0500

    chore: update doc files

--- docs/assets/js/aab35a30.cfceb5f1.js
+++ docs/assets/js/aab35a30.cfceb5f1.js
@@ -1,9 +1,1 @@
-'use strict';
-(self.webpackChunkurvo_docs = self.webpackChunkurvo_docs || []).push([
-	[6075],
-	{
-		3769: (s) => {
-			s.exports = JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}');
-		},
-	},
-]);
+"use strict";(self.webpackChunkurvo_docs=self.webpackChunkurvo_docs||[]).push([[6075],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
