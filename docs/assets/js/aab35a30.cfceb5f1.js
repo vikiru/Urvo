81c09f16baf3120cb353c27b1832d0f5dca3243a
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-14 20:07:43 -0500

    Updates

--- docs/assets/js/aab35a30.cfceb5f1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(self.webpackChunkurvo_docs = self.webpackChunkurvo_docs || []).push([
-	[6075],
-	{
-		3769: (s) => {
-			s.exports = JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}');
-		},
-	},
-]);
--- test/test.js
+++
@@ -1,4 +0,0 @@
-require('./image/image.test');
-require('./info/info.test');
-require('./meme/meme.test');
-require('./random/random.test');
